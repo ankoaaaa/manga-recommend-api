39bfc9a9f84b134465654f0436114894e9adfad7
ankoaaaa <sagiya.kouta@gmail.com>
2025-07-11 20:14:34 +0900

    change config

--- src/main/java/io/ankoaaaa/mangaRecommendApi/config/WebConfig.java
+++ src/main/java/io/ankoaaaa/mangaRecommendApi/config/WebConfig.java
@@ -1,18 +1,23 @@
 package io.ankoaaaa.mangaRecommendApi.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+	
+    // application.propertiesまたは環境変数から値を取得
+    @Value("${CORS_ALLOWED_ORIGINS}")
+    private String allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // すべてのエンドポイントを対象にする
-                .allowedOrigins("http://localhost:5173") // 許可するオリジン（フロントエンドのURL）
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 許可するHTTPメソッド
-                .allowedHeaders("*") // 許可するヘッダー
+                .allowedOrigins(allowedOrigins) // 環境変数で設定したURLを許可
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
